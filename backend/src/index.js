5d3fcec2e54ecf40313cdc0792819ce7ef8929f3
Arthur YVARS <aodarty23@gmail.com>
2024-05-16 16:48:52 +0200

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const TelegramBot = require('node-telegram-bot-api');
+const cors = require('cors'); // Importez le module CORS
+
+const token = '7045386731:AAGSmPQQ_t2Po5eDob7rUEWEZ25W-s2t9fg';
+const app = express();
+const bot = new TelegramBot(token, {polling: true});
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Content-Security-Policy', "default-src 'self'; connect-src 'self' http://localhost:5174");
+    next();
+});
+
+app.use(cors());
+
+app.post('/send-message', (req, res) => {
+    const { message } = req.body;
+    const chatId = '6064290146';
+
+    bot.sendMessage(chatId, message)
+        .then(() => {
+            res.status(200).send('Message sent successfully!');
+        })
+        .catch((error) => {
+            console.error('Error sending message:', error);
+            res.status(500).send('An error occurred while sending the message');
+        });
+});
+
+const port = 5174;
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function App() {
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            console.log(message);
+            await axios.post('http://localhost:5174/send-message', { message });
+            setMessage('');
+            console.log('Message envoyé avec succès !');
+        } catch (error) {
+            console.error('Erreur lors de l\'envoi du message :', error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Send Message to Telegram</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="message">Message:</label><br/>
+                <input type="text" id="message" name="message" value={message} onChange={(event) => setMessage(event.target.value)} /><br/>
+                <button type="submit">Send</button>
+            </form>
+        </div>
+    );
+}
+
+export default App;
