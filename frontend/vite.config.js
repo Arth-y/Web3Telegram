7fe7a1335ab5bf28f62517a37d410da463047253
Arthur YVARS <aodarty23@gmail.com>
2024-05-17 15:11:12 +0200

    Vite config update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,4 @@
-import react from '@vitejs/plugin-react';
+/*import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
@@ -12,4 +12,12 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [react()]
   };
-});
+});*/
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
